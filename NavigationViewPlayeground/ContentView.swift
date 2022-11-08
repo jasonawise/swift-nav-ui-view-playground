3f90892bcca64ce88695db9fb15d1f4094989a7a
Jason A. Wise <jasonandrewwise@gmail.com>
2022-11-07 20:42:12 -0500

    init commit

--- NavigationViewPlayeground/ContentView.swift
+++ NavigationViewPlayeground/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+      NavigationView {
+        NavigationLink(destination: Text("Second View"))
+        {
+          Text("Hello I am a nav view")
         }
-        .padding()
+          .navigationTitle("Nav View")
+          .toolbar {
+            ToolbarItem(placement: .bottomBar) {
+              NavigationLink( destination: SettingsView()) {
+                Text("Settings")
+              }
+            }
+          }
+      }
     }
 }
 
