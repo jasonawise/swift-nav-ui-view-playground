a28387367c29cebb598f302230d1c09c2f2b6bd8
Jason A. Wise <jasonandrewwise@gmail.com>
2022-11-07 20:09:01 -0500

    Initial Commit

---
+++ NavigationViewPlayeground/NavigationViewPlayegroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewPlayegroundApp.swift
+//  NavigationViewPlayeground
+//
+//  Created by Jason Wise on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewPlayegroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
